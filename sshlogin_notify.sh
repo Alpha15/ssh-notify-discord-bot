c280ad66cee2cfa686eca9ec273b0975d1859698
Alpha15 <kobayashi-kikoushi@xmail.plala.or.jp>
2023-08-29 10:21:53 +0900

    add output for wrong protocol

--- sshlogin_notify.sh
+++ sshlogin_notify.sh
@@ -21,24 +21,38 @@ if [ -f ${LOGFILE_NAME} ]; then
 	mv ${LOGFILE_NAME} /tmp/tmp_auth.log
 	service rsyslog restart
 
+	# Login success
 	cat /tmp/tmp_auth.log | grep "sshd\[[0-9]*\]" | grep "Accepted" > /tmp/tmp_login.log
 	cat /tmp/tmp_login.log | while read line
-do
-	IP=$(echo $line | cut -f 11 -d " ")
-	USER=$(echo $line | cut -f 9 -d " ")
-	date_time $line
-	echo "${IP}:allow to access as ${USER}.(${TIME})"
-done
+	do
+		IP=$(echo $line | cut -f 11 -d " ")
+		USER=$(echo $line | cut -f 9 -d " ")
+		date_time $line
+		echo "${IP}:allow to access as ${USER}.(${TIME})"
+	done
 
+	# Login failed (wrong auth)
 	cat /tmp/tmp_auth.log | grep "sshd\[[0-9]*\]" | grep "preauth" > /tmp/tmp_nologin.log
 	cat /tmp/tmp_nologin.log | while read line
-do
-	IP=$(echo $line | cut -f 12 -d " ")
-	USER=$(echo $line | cut -f 11 -d " ")
-	date_time $line
-	echo "${IP}:denied to access as ${USER}.(${TIME})"
-done
+	do
+		IP=$(echo $line | cut -f 12 -d " ")
+		USER=$(echo $line | cut -f 11 -d " ")
+		date_time $line
+		echo "${IP}:denied to access as ${USER}.(${TIME})"
+	done
 
-	rm /tmp/tmp_auth.log /tmp/tmp_login.log /tmp/tmp_nologin.log
+	# Login failed (wrong protocol)
+	cat /tmp/tmp_auth.log | grep "sshd\[[0-9]*\]" | grep "Did not receive identification string" > /tmp/tmp_noidentification.log
+	cat /tmp/tmp_noidentification.log | while read line
+	do
+		IP=$(echo $line | cut -f 12 -d " ")
+		date_time $line
+		echo "Did not receive identification string from ${IP}.(${TIME})"
+	done
+
+	rm /tmp/tmp_auth.log
+	rm /tmp/tmp_login.log
+	rm /tmp/tmp_nologin.log
+	rm /tmp/tmp_noidentification.log
 
 fi

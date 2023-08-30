aba95eaa46be6fc5fe55d5160119f6cd3763a121
Alpha15 <kobayashi-kikoushi@xmail.plala.or.jp>
2023-08-30 20:10:18 +0900

    fix a little

--- utils.py
+++ utils.py
@@ -5,4 +5,6 @@ def coloredString(message):
         ReturnStr = '```diff\n+ {}\n```'.format(message)
     if ':denied to' in message:
         ReturnStr = '```diff\n- {}\n```'.format(message)
+    if 'Did not receive' in message:
+        ReturnStr = '```diff\n- {}\n```'.format(message)
     return ReturnStr
